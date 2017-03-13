9adc2354dd8c75457a9af9113b7efe3ff4c388ce
Kristofer Krause <kris.krause@gmail.com>
2017-03-13 10:17:23 -0400

    dones & totals; init char

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 $(document).foundation();
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: [
+            "Expenses",
+            "Savings",
+            "Income"
+        ],
+        datasets: [
+            {
+                data: [60, 10, 40],
+                backgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ],
+                hoverBackgroundColor: [
+                    "#FF6384",
+                    "#36A2EB",
+                    "#FFCE56"
+                ]
+            }]
+    }
+});
--- src/renderer.js
+++ src/renderer.js
@@ -8,12 +8,56 @@ var db = new JsonDB("mydata", true, true);
 var viewModel = {};
 
 viewModel.monthlyincome = ko.observableArray(db.getData("/monthly-income"));
+viewModel.monthlyincomeTotal = ko.pureComputed(function () {
+    var total = 0;
+    ko.utils.arrayForEach(this.monthlyincome(), function (item) {
+        var value = parseFloat(item.amount);
+        if (!isNaN(value)) {
+            total += value;
+        }
+    });
+    return total.toFixed(2);
+}, viewModel);
+
 viewModel.monthlyexpenses = ko.observableArray(db.getData("/monthly-expenses"));
+viewModel.monthlyexpensesTotal = ko.pureComputed(function () {
+    var total = 0;
+    ko.utils.arrayForEach(this.monthlyexpenses(), function (item) {
+        var value = parseFloat(item.amount);
+        if (!isNaN(value)) {
+            total += value;
+        }
+    });
+    return total.toFixed(2);
+}, viewModel);
+
 viewModel.monthlysavings = ko.observableArray(db.getData("/monthly-savings"));
+viewModel.monthlysavingsTotal = ko.pureComputed(function () {
+    var total = 0;
+    ko.utils.arrayForEach(this.monthlysavings(), function (item) {
+        var value = parseFloat(item.amount);
+        if (!isNaN(value)) {
+            total += value;
+        }
+    });
+    return total.toFixed(2);
+}, viewModel);
+
 viewModel.monthlydebts = ko.observableArray(db.getData("/monthly-debts"));
+viewModel.monthlydebtsTotal = ko.pureComputed(function () {
+    var total = 0;
+    ko.utils.arrayForEach(this.monthlydebts(), function (item) {
+        var value = parseFloat(item.amount);
+        if (!isNaN(value)) {
+            total += value;
+        }
+    });
+    return total.toFixed(2);
+}, viewModel);
 
 viewModel.savings = ko.observableArray(db.getData("/savings"));
 viewModel.assets = ko.observableArray(db.getData("/assets"));
 viewModel.debts = ko.observableArray(db.getData("/debts"));
 
 ko.applyBindings(viewModel);
+
